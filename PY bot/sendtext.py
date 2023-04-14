ff1cfe9b9f73e125f92281242a55066302295278
regraced <71679194+regraced@users.noreply.github.com>
2023-04-13 21:54:24 -0500

    Update sendtext.py

--- PY bot/sendtext.py
+++ PY bot/sendtext.py
@@ -78,7 +78,7 @@ def run_script():
             else:
                 print('\n\nno update\n')
 
-            time.sleep(1800)
+            time.sleep(18000)
 
 def run_flask():
     port = int(os.environ.get('PORT', 5000))
